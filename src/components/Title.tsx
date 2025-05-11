200066e3ed88702843023320fc00f6e5a188cfb2
Angela Cieslak <161291402+cieslak96@users.noreply.github.com>
2025-05-11 17:01:32 +0000

    border

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -30,14 +30,14 @@ export default function Title({ title }: TitleProps) {
       style={{ fontFamily: 'var(--font-cinzel)' }}
     >
       <div
-        className="h-[2px] bg-green-900 mx-auto mb-4"
+        className="h-[2px] bg-[#1e1e1e] mx-auto mb-4"
         style={{ width: `${borderWidth}px` }}
       />
       <h1 ref={titleRef} className="text-4xl  text-[#1e1e1e] inline-block">
         {title}
       </h1>
       <div
-        className="h-[2px] bg-green-900 mx-auto mt-4"
+        className="h-[2px] bg-[#1e1e1e] mx-auto mt-4"
         style={{ width: `${borderWidth}px` }}
       />
     </div>
