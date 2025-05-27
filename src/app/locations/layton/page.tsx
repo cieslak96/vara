597fc0c40378af327ead6c6b794055e442d761d6
Rose <rose@Roses-MacBook-Air.local>
2025-05-26 21:58:51 -0600

    iwd

--- src/app/locations/layton/page.tsx
+++ src/app/locations/layton/page.tsx
@@ -12,26 +12,30 @@ export default function TwoColumnPage() {
     <>
       <Header />
     
-   <main>
-  <Title title="Layton" />
+      <main >
+        <Title title="Layton" />
 
-  <div className="w-full flex flex-col lg:flex-row px-4 lg:px-0 min-h-screen">
-    {/* Left Column (Visuals) */}
-    <div className="w-full lg:w-1/4 relative flex flex-col overflow-hidden">
-      <div className="relative w-full aspect-[3/4] lg:h-full">
-        <LocationsVisuals />
-      </div>
-    </div>
-
-    {/* Right Column (Floor Plan) */}
-    <div className="w-full lg:w-3/4 bg-white flex flex-col">
-      <div className="flex-grow space-y-10 py-10">
-        <FloorPlan />
-      </div>
+<div className="w-full flex flex-col lg:flex-row px-4 lg:px-0">
+  {/* Left Column (Visuals) */}
+  <div className="w-full lg:w-1/4 relative flex flex-col ">
+    <div className="relative w-full lg:aspect-[3/4] lg:aspect-auto lg:flex-1">
+      <LocationsVisuals />
     </div>
   </div>
-</main>
 
+  {/* Right Column (Floor Plan) */}
+  <div className="w-full lg:w-3/4 bg-white flex flex-col">
+    <div className="flex-grow space-y-10  py-10 ">
+      <FloorPlan />
+    </div>
+  </div>
+</div>
+
+
+
+
+
+      </main>
       
       <Footer />
     </>
